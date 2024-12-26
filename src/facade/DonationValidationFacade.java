c289cc28cfdc243bc3fdc3e3f0da6a4675772949
fadhel-rizanda <128169066+fadhel-rizanda@users.noreply.github.com>
2024-12-26 10:12:21 +0700

    Update DonationValidationFacade.java

--- src/facade/DonationValidationFacade.java
+++ src/facade/DonationValidationFacade.java
@@ -143,4 +143,71 @@ public class DonationValidationFacade {
         } while (cancelProcess.equalsIgnoreCase("Y"));
         System.out.println("Your Cancelation Process Has Been Terminated");
     }
+
+	    public void cancelDonation() {
+        Vector<Donation> cancelDonationList = database.readDonationList();
+        String cancelDonationID;
+        String cancelProcess = "N";
+
+	 Vector<Donation> viewDonationList = database.readDonationList();
+        MoneyAdapter moneyAdapter = new MoneyAdapter();
+        if (viewDonationList.isEmpty()) {
+            System.out.println("No Donations Made Yet");
+            return;
+        } else {
+            System.out.println("=============================================================================================================================================================================");
+            System.out.printf("|| %-167s ||\n","                                                                   GoodsDeed Donation List");
+            System.out.println("=============================================================================================================================================================================");
+            System.out.printf("|| %-2s || %-12s || %-20s || %-15s || %-15s || %-20s || %-30s || %-25s ||\n",
+                                "No", "Donation ID", "    Donator Name", "Donator Contact", " Donation Type", "Donation Description", "        Donation Ammount", "    Donation Message");
+            System.out.println("=============================================================================================================================================================================");
+
+            int no = 1;
+
+            for (Donation donation : viewDonationList) {
+                String format = "|| %-2d || %-12s || %-20s || %-15s || %-15s || %-20s || %-30s || %-25s ||\n";
+                if (donation.getDonationType().equals("Money")) {
+                   if(donation.getDonationDescription().equals("Dollar") || donation.getDonationDescription().equals("Euro")) {
+                	   System.out.printf(format, no++, donation.getDonationID(), donation.getDonatorName(), donation.getDonatorContact(), donation.getDonationType(), donation.getDonationDescription(), donation.getDonationAmount() 
+                			   + " (Rp " + moneyAdapter.convertToRupiah(donation.getDonationDescription(), donation.getDonationAmount()) + ")", donation.getDonationMessage());
+                   }
+                   else {
+                	   System.out.printf(format, no++, donation.getDonationID(), donation.getDonatorName(), donation.getDonatorContact(), donation.getDonationType(), donation.getDonationDescription(),
+                			   "Rp " + donation.getDonationAmount(), donation.getDonationMessage());
+                   }
+                } else {
+                    System.out.printf(format, no++, donation.getDonationID(), donation.getDonatorName(), donation.getDonatorContact(), donation.getDonationType(), donation.getDonationDescription(), 
+                    		+donation.getDonationAmount() + " items", donation.getDonationMessage());
+                }
+            }
+
+            System.out.println("=============================================================================================================================================================================");        
+        }
+
+        if (cancelDonationList.isEmpty()) {
+            return;
+        }
+        System.out.println();
+        do {            
+            System.out.print("Input Donation ID to cancel donation: ");
+            cancelDonationID = scan.nextLine();
+
+            for (Donation donation : cancelDonationList) {
+                if (cancelDonationID.equals(donation.getDonationID())) {
+                    database.deleteDonationList(cancelDonationID);
+                    System.out.println("Your Cancelation Process Succeed");
+                    System.out.println("Your Donation Has Been Canceled");
+                    return;
+                }
+            }
+
+            System.out.println("Invalid Donation ID. Please try again");
+            do {
+                System.out.print("Do you want to continue Process [Y|N]: ");
+                cancelProcess = scan.nextLine();
+            } while (!(cancelProcess.equals("Y") || cancelProcess.equals("N")));
+
+        } while (cancelProcess.equalsIgnoreCase("Y"));
+        System.out.println("Your Cancelation Process Has Been Terminated");
+    }
 }
